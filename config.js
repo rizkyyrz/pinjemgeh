bc83d2d3250a8db58dc1d7a1c4adc51d066537b9
Muhammad Rizky Ramadhan <51981145+rizkyyrz@users.noreply.github.com>
2026-03-26 23:55:51 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  // Ganti ke domain Worker kamu kalau sudah pakai custom domain.
+  // Contoh: 'https://api.pinjemgeh.my.id'
+  API_BASE_URL: 'https://pinjemgeh.riezkyramadhan41.workers.dev'
+};
