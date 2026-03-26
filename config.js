60b408a5fec83a8c31646127f0b5f132677f774f
Muhammad Rizky Ramadhan <51981145+rizkyyrz@users.noreply.github.com>
2026-03-27 00:23:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 window.APP_CONFIG = {
-  // Ganti ke domain Worker kamu kalau sudah pakai custom domain.
-  // Contoh: 'https://api.pinjemgeh.my.id'
-  API_BASE_URL: 'https://pinjemgeh.riezkyramadhan41.workers.dev'
+  API_BASE_URL: "https://pinjemgeh.my.id/api"
 };
